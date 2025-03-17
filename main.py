d8bfb5e19b63938ca43321a1ab19bdbb074d812f
hartske <keatoru@gmail.com>
2025-03-16 20:20:15 -0700

    changed update/draw calls

--- main.py
+++ main.py
@@ -6,18 +6,26 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
+        updateable.update(dt)
+
         screen.fill(000000)
 
-        player.draw(screen)
-        player.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
 
